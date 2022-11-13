74dd89d4d3f078a3045c31ee9f1d9da347ba7f57
Anil Yerramasu <anilyerramasu@Anils-MacBook-Pro.local>
2022-11-13 19:57:57 +0530

    update app.py

--- src/FasterRCNN/App.py
+++ src/FasterRCNN/App.py
@@ -47,9 +47,9 @@ def predict_batch(predictor: DefaultPredictor, im_list: List[ndarray]) -> List:
 
 
 if __name__ == '__main__':
-    inputdir = Path("./")
+    # inputdir = Path("./")
     traineddir = inputdir / "results/v9"
-    flags: Flags = Flags().update(load_yaml(str(traineddir / "flags.yaml")))
+    flags: Flags = Flags().update(load_yaml(str("." / "flags.yaml")))
     debug = flags.debug
     cfg = get_cfg()
     # inputdir = Path("./")
